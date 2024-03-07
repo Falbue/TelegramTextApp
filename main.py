93bbfaae788a03c91860288fe3edacefae184e91
Falbue <cyansair05@gmail.com>
2024-03-08 03:10:52 +0500

    функция создания клавиатур

--- main.py
+++ main.py
@@ -52,3 +52,23 @@ def now_time():
     date = f"{current_date} {current_time}"
     return date
 
+
+def create_keyboard(buttons):
+    keyboard = InlineKeyboardMarkup(row_width = 2)
+    btn_return = InlineKeyboardButton(text = 'Назад', callback_data = 'return')
+    row_buttons = []
+    num_buttons = 0
+    for i in range(len(buttons)):
+        button_text = (buttons[i]).split('.')[0]
+        button_callback_data = f"{button_text}_data"
+        button = InlineKeyboardButton(text = button_text, callback_data = button_callback_data)
+        row_buttons.append(button)
+        num_buttons += 1
+        if num_buttons == 2:
+            keyboard.add(*row_buttons)
+            row_buttons = []
+            num_buttons = 0
+    if num_buttons > 0:
+        keyboard.add(*row_buttons)
+    keyboard.add(btn_return)
+    return keyboard
