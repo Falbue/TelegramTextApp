217a637fa047bd6f97ee63b23092315c279f81ad
Falbue <cyansair05@gmail.com>
2024-03-08 03:08:34 +0500

    добавление библиотек

--- main.py
+++ main.py
@@ -2,3 +2,14 @@
 bot_api = '' # бот для тестов
 folder = 'data'
 # folder = '/data'
+import os
+import sqlite3
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+import telebot
+import re
+from datetime import datetime
+import pytz
+from flask import Flask
+import sys
+sys.path.append(folder)
+import threading
