d217f99fab438264b8b75a8102f63edcb36ddef1
Falbue <cyansair05@gmail.com>
2024-03-08 03:10:06 +0500

    проверка нужных файлов

--- main.py
+++ main.py
@@ -19,3 +19,26 @@ folder_path = f"{folder}"
 texts_path = f"{folder}/texts"
 db_name = "database.db"
 db_path = os.path.join(folder_path, db_name)
+
+# основные функции
+def main_check():
+    if not os.path.exists(f"{folder}"):
+        os.makedirs(f"{folder}")
+        print("Папка библиотеки создана")
+    if not os.path.exists(texts_path):
+        os.makedirs(texts_path)
+        print("Папка текстов создана")    
+    if os.path.exists(db_path):
+        print("База данных существует")
+    else:
+        conn = sqlite3.connect(f"{folder}/database.db")
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE users (
+                user_id INTEGER,
+                course INTEGER,
+                groupe INTEGER,
+                time_registration INTEGER,
+                id_message INTEGER);
+            ''')
+        conn.close()
