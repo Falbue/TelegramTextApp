e75b573e27a335b0359f77dfc928e8327103ba77
Falbue <cyansair05@gmail.com>
2024-06-06 01:30:56 +0500

    Начало работы

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
-bot_api = ''
+bot_api = '6373877428:AAHJ3oFfoeXY6pK82PLHwA_DtdwjBN44JQ8'
 folder = 'data'
-id_admin = 1
+id_admin = 1210146115
 # -----------------------------------------------------------
 
 
@@ -16,3 +16,96 @@ import re
 sys.path.append(folder)
 import threading
 bot = telebot.TeleBot(bot_api)
+
+
+# пути
+folder_path = f"{folder}"
+db_name = "database.db"
+db_path = os.path.join(folder_path, db_name)
+texts_path = f"{folder}/texts"
+menu_user_path = f'{folder}/user_menu'
+menu_dev_path = f'{folder}/telegram_text_apps_menu'
+error_path = f'{texts_path}/error_log.txt'
+
+
+
+# основные функции
+def main_check():   # основные проверки
+    if not os.path.exists(f"{folder}"):
+        os.makedirs(f"{folder}")
+        print("Папка библиотеки создана")
+    if not os.path.exists(texts_path):
+        os.makedirs(texts_path)
+        print("Папка текстов создана")
+    if not os.path.exists(menu_user_path):
+        os.makedirs(menu_user_path)
+        print("Папка с пользовательскими меню создана")
+    if not os.path.exists(menu_dev_path):
+        os.makedirs(menu_dev_path)
+        print("Папка c меню приложения создана")     
+    if not os.path.exists(error_path):
+        with open(error_path, 'w'): 
+            pass
+        print("Файл с логами ошибок создан")
+    if not os.path.exists(db_path):
+        conn = sqlite3.connect(f"{folder}/database.db")
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE users (
+                user_id INTEGER,
+                time_registration INTEGER,
+                id_message INTEGER);
+            ''')
+        conn.close()
+        print("База данных создана")
+
+def now_time(): # получение текущего времени
+    now = datetime.now()
+    tz = pytz.timezone('Europe/Moscow')
+    now_moscow = now.astimezone(tz)
+    current_time = now_moscow.strftime("%H:%M")
+    current_date = now_moscow.strftime("%m.%d.%Y")
+    date = f"{current_date} {current_time}"
+    return date
+
+
+@bot.message_handler(commands=['start'])
+def start(message): # обработка команды start
+    message_id = message.id
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+    c.execute("SELECT * FROM users WHERE user_id=?", (message.chat.id,))
+    result = c.fetchone()
+    if result is None:
+        registration = now_time()
+        c.execute("INSERT INTO users (user_id, time_registration, id_message) VALUES (?, ?, ?)",
+                  (message.chat.id, registration, message_id+1))
+        conn.commit()
+        print('Зарегистрирован новый пользователь', message.chat.id)
+    else:
+        c.execute(f"UPDATE users SET id_message = {(message_id+1)} WHERE user_id = {message.chat.id}")
+        print('Пользователь' ,message.chat.id,'уже существует в базе')
+        conn.commit()
+        pass
+    conn.close()
+
+    bot.delete_message(chat_id=message.chat.id, message_id=message.message_id)
+    bot.send_message(message.chat.id, "Главное меню")
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    if call.data == 'data_start': 
+        open_menu(name = 'main', call = call)
+
+    
+
+main_check()
+print("Бот запущен...")
+
+try:
+    bot.polling()
+except Exception as e:
+    with open(error_path, 'w+', encoding='utf-8') as f:
+        f.write(str(e) + "\n")
+        goodbuy
