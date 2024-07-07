786776fe680416b987d2396ef88f987d288d2da2
Falbue <cyansair05@gmail.com>
2024-07-07 20:09:23 +0500

    Добавлены настройки установки

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import os
+
+# Читаем README.md для long_description
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='TelegramTextApp',
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'pyTelegramBotAPI',
+        'pytz',
+    ],
+    # Добавляем long_description и long_description_content_type
+    ng_description=long_description,
+    long_description_content_type='text/markdown',
+    author="Falbue",
+    author_email="cyansair05@gmail.com",
+    description="Telegram Text App",
+    url="https://github.com/Falbue/TelegramTextApp",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
