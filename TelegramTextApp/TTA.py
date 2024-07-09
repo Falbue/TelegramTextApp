d4ce86359d9d10c745f69496240c1a6994e0e692
Falbue <cyansair05@gmail.com>
2024-07-09 23:34:16 +0500

    правильно указан путь

--- TelegramTextApp/TTA.py
+++ TelegramTextApp/TTA.py
@@ -8,13 +8,15 @@ def start():
         print("Usage: TTA <NAME-PROJECT> or TTA ALL")
         sys.exit(1)
 
-    with open('TTA_dir.txt', 'r') as file:
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    with open(f'{script_dir}/TTA_dir.txt', 'r') as file:
         tta_path = file.readline().strip()
     NAME = sys.argv[1]
 
     if sys.argv[2] == "ALL": 
         for entry in os.listdir(tta_path):
             if os.path.isdir(os.path.join(tta_path, entry)):
-                os.system(f'python {script_path}/{entry}/bot.py')
+                os.system(f'python {tta_path}/{entry}/bot.py')
     else:
-        os.system(f'python {script_path}/{NAME}/bot.py')
+        os.system(f'python {tta_path}/{NAME}/bot.py')
