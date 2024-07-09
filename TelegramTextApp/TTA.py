5df9d489dcd377681f8ba72e780922643ffcceec
Falbue <cyansair05@gmail.com>
2024-07-09 13:02:46 +0500

    запуск TTA теперь будет из консоли

--- TelegramTextApp/TTA.py
+++ TelegramTextApp/TTA.py
@@ -1,21 +1,11 @@
+import sys
 import os
-import subprocess
+import sqlite3
+import shutil
 
 def start():
-    # Определяем директорию, в которой находится скрипт
-    base_directory = os.path.dirname(os.path.abspath(__file__))
+    if len(sys.argv) != 2:
+        print("Usage: TTA <NAME PROJECT>")
+        sys.exit(1)
 
-    # Проходим по всем элементам в указанной директории
-    for item in os.listdir(base_directory):
-        item_path = os.path.join(base_directory, item)
-        
-        # Проверяем, является ли элемент папкой
-        if os.path.isdir(item_path):
-            bot_script_path = os.path.join(item_path, 'bot.py')
-            
-            # Проверяем, существует ли скрипт bot.py в этой папке
-            if os.path.exists(bot_script_path):
-                print(f"Запуск скрипта {bot_script_path}")
-                # Запускаем скрипт bot.py
-                subprocess.run(['python', bot_script_path])
-
+start()
